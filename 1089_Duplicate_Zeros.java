7d682226e7d0152513ab27cad8a68e85527d10ce
Akshay Patel <98717856+amp-patel@users.noreply.github.com>
2022-02-02 22:09:32 +0000

    Create 1089_Duplicate_Zeros.java

---
+++ 1089_Duplicate_Zeros.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        for(int i=0; i<arr.length;i++)
+        {
+            if(arr[i]==0)
+            {
+                for(int j = arr.length-1; j>i;j--)
+                {
+//                    System.out.println(arr[j]);
+
+                    arr[j]=arr[j-1];
+//                    System.out.println(arr[j]);
+
+                }
+                if(i+1<arr.length) {
+                    arr[i + 1] = 0;
+                }
+//                arr[i+1]=0;
+                i++;
+            }
+//            System.out.println(arr[i]);
+
+        }
+
+        for(int i=0; i<arr.length;i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
